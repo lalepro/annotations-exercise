1b7dd86d3ab706552c9a4ee169a83be4ff73b852
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-03-09 10:38:34 -0600

    fixed all the warnings using the suppressWarning

--- src/Person.java
+++ src/Person.java
@@ -9,7 +9,7 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-
+    @SuppressWarnings({"deprecation", "unchecked"})
     public String sortName() {
         return String.format("%s %s", firstName, lastName);
     }
